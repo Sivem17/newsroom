2dfa5aa0d4b17631322877264577f27327f49281
Sivem17 <36225367+Sivem17@users.noreply.github.com>
2018-10-26 23:58:24 -0300

    Update GradeDistribution.java

--- GradeDistribution.java
+++ GradeDistribution.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package a06;
 
 /**
@@ -11,11 +6,112 @@ package a06;
  */
 public class GradeDistribution {
 
-    private int reqGrade; 
-    private String numA, numB, numC, numD, numF;
+    //DECLARING INSTANCE VARIABLE
+    private int numA, numB, numC, numD, numF, total;
 
-    void print() {
+    /**
+     * 
+     * @param numberGrade is given a condition in which it will show number of each variables.
+     */
+    //METHOD FOR CONVERTING NUMBERS INTO GRADES    
+    public void addGrade(int numberGrade) {
+        if (numberGrade >= 80) {
+            numA++;
+        } else if (numberGrade >= 70) {
+            numB++;
+        } else if (numberGrade >= 60) {
+            numC++;
+        } else if (numberGrade >= 50) {
+            numD++;
+        } else {
+            numF++;
+        }
+        total = numA + numB + numC + numD + numF;
 
+    }
+    /**
+     * 
+     * @return numA
+     */
+    //GETTER RETURNS numA
+    public int getAs() {
+        return numA;
+    }
+    /**
+     * 
+     * @return percentage of grade A
+     */
+    double percentAs() {
+        return numA / (double) total * 100;
+    }
+    /**
+     * 
+     * @return numB
+     */
+    public int getBs() {
+        return numB;
+    }
+    /**
+     * 
+     * @return percentage of grade B
+     */
+    public double percentBs() {
+        return numB / (double) total * 100;
+    }
+    /**
+     * 
+     * @return numC
+     */
+    public int getCs() {
+        return numC;
+    }
+    /**
+     * 
+     * @return percentage of grade C
+     */
+    public double percentCs() {
+        return numC / (double) total * 100;
+    }
+    /**
+     * 
+     * @return numD
+     */
+    public int getDs() {
+        return numD;
+    }
+    /**
+     * 
+     * @return percentage of grade D
+     */
+    public double percentDs() {
+        return numD / (double) total * 100;
+    }
+    /**
+     * 
+     * @return numF
+     */
+    public int getFs() {
+        return numF;
+    }
+    /**
+     * 
+     * @return percentage of grade F
+     */
+    public double percentFs() {
+        return numF / (double) total * 100;
+    }
+    /**
+     * 
+     * @return total
+     */
+    public int getTotal() {
+        return total;
+    }
+    /**
+     * Method for printing the bar graph.
+     */
+    public void print() {
+        //LOOP FOR PRINTING THE 1ST PART OF SCALE
         System.out.println();
         for (int x = 0; x <= 100; x = x + 10) {
             System.out.print(x);
@@ -23,6 +119,7 @@ public class GradeDistribution {
                 System.out.print(" ");
             }
         }
+        //LOOP FOR PRINTING THE 2ND PART OF SCALE
         System.out.println();
         for (int j = 0; j <= 100; j = j + 10) {
             System.out.print("|");
@@ -31,69 +128,45 @@ public class GradeDistribution {
             }
         }
         System.out.println();
-    }
-
-    void reset() {
-    }
-
-    public String addGrade(int numberGrade) {
-        this.reqGrade=numberGrade;
-        if (numberGrade >= 80) {
-            return "A";
-        } else if (numberGrade >= 70) {
-            return "B";
-        } else if (numberGrade >= 60) {
-            return "C";
-        } else if (numberGrade >= 50) {
-            return "D";
-        } else {
-            return "F";
-        }
         
+        System.out.print("A");
+        for (int i = 0; i < ((numA / (double) total * 100) * 6 / 10); i++) {
+            System.out.print("*");
+        }
+        System.out.println();
 
+        System.out.print("B");
+        for (int i = 0; i < ((numB / (double) total * 100) * 6 / 10); i++) {
+            System.out.print("*");
+        }
+        System.out.println();
+
+        System.out.print("C");
+        for (int i = 0; i < ((numC / (double) total * 100) * 6 / 10); i++) {
+            System.out.print("*");
+        }
+        System.out.println();
+
+        System.out.print("D");
+        for (int i = 0; i < ((numD / (double) total * 100) * 6 / 10); i++) {
+            System.out.print("*");
+        }
+        System.out.println();
+
+        System.out.print("F");
+        for (int i = 0; i < ((numF / (double) total * 100) * 6 / 10); i++) {
+            System.out.print("*");
+        }
+    }
+    /**
+     * Method for resetting all the variables.
+     */
+    public void reset() {
+        numA = 0;
+        numB = 0;
+        numC = 0;
+        numD = 0;
+        numF = 0;
     }
 
-    String getAs() {
-        return numA;
-    }
-
-    String percentAs() {
-        return addGrade(reqGrade);
-    }
-
-    String getBs() {
-        return numB;
-    }
-
-    String percentBs() {
-        return addGrade(reqGrade);
-    }
-
-    String getCs() {
-        return numC;
-    }
-
-    String percentCs() {
-        return addGrade(reqGrade);
-    }
-
-    String getDs() {
-        return numD;
-    }
-
-    String percentDs() {
-        return addGrade(reqGrade);
-    }
-
-    String getFs() {
-        return numF;
-    }
-
-    String percentFs() {
-        return addGrade(reqGrade);
-    }
-
-    String getTotal() {
-        return "";
-    }
 }
